4c29cf0a93a073468b62acb02fdb2172abac4fef
Michal Zielinski <michal@zielinscy.org.pl>
2014-04-09 21:21:41 +0200

    color-theme, doremi

--- _.emacs.d/init.el
+++ _.emacs.d/init.el
@@ -180,12 +180,18 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/solarized")
 (add-to-list 'load-path "~/.emacs.d/solarized")
 
+(require 'color-theme)
+(setq color-theme-is-global t)
+;(eval-after-load "color-theme" '(color-theme-hober))
+
+;(require 'doremi-cmd)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (manoj-dark)))
+ '(custom-enabled-themes nil)
  '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(help-at-pt-display-when-idle (quote (flymake-overlay)) nil (help-at-pt))
  '(help-at-pt-timer-delay 0.5))
